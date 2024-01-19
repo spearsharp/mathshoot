f6c3a50fef9a0c66ce4c3a35b847d453e33b1cd6
yaospear <yaospear@163.com>
2024-01-19 23:52:39 +0800

    util gen fixed

--- lib/services/util.dart
+++ lib/services/util.dart
@@ -1,5 +1,7 @@
+import 'dart:ffi';
 import 'dart:math';
 
+import 'package:get/get.dart';
 import 'package:uuid/uuid.dart';
 
 class Tools {
@@ -10,19 +12,84 @@ class Tools {
   }
 
   static uName<String>(int v) {
-    var randomString;
-    Random random = Random();
+    String randomStringHead = "MS-" as String;
+    String randomName = '' as String;
+
     if (v > 10) {
-      randomString = "LengthErr";
+      randomName = "LengthErr" as String;
     } else {
-      const characters =
-          '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-      for (int i = 0; i < v; i++) {
-        int index = random.nextInt(v);
-        String char = characters[index] as String;
-        randomString += char;
+      Random random = Random();
+      List characters = [
+        '0',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z',
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z'
+      ];
+      for (int i = 0; i < v!; i++) {
+        String tmpchar = characters[Random().nextInt(characters.length)];
+        randomStringHead =
+            (randomStringHead!.toString() + tmpchar!.toString())! as String;
       }
     }
-    return randomString;
+    randomName = randomStringHead;
+    return randomName;
   }
 }
